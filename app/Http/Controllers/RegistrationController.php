024cd3fa742d5fb128baf589f259fd006c926690
HenriqueCardoso-Liceu <tecnologia3@liceubrasil.com.br>
2026-08-04 10:50:22 -0300

    fix namespace

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use App\Actions\CreateRegistrationAction;
 use App\DTOs\CreateRegistrationDTO;
---
+++ tests/Feature/RegistrationRouteTest.php
@@ -0,0 +1,7 @@
+<?php
+
+test('registration route resolves the controller', function () {
+  $response = $this->postJson('/api/registrations', []);
+
+  $response->assertStatus(422);
+});
