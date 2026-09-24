bcde86851287092790782e02ee152069ff1dc860
HenriqueCardoso-Liceu <tecnologia3@liceubrasil.com.br>
2026-09-24 09:56:06 -0300

    fix migration

--- database/migrations/2026_09_23_192054_create-matriculas2027-col-itaqua-table.php
+++ database/migrations/2026_09_23_192054_create-matriculas2027-col-itaqua-table.php
@@ -13,9 +13,18 @@ return new class extends Migration
     {
         //
         Schema::create('matriculas2027-col-itaqua', function (Blueprint $table) {
+            $table->id();
             $table->string('responsible_name', 150)->nullable();
             $table->string('mobile_phone', 25)->nullable();
             $table->string('interest', 150)->nullable();
+            $table->string('utm_source')->nullable();
+            $table->string('utm_medium')->nullable();
+            $table->string('utm_campaign')->nullable();
+            $table->string('utm_term')->nullable();
+            $table->string('utm_content')->nullable();
+            $table->string('referrer')->nullable();
+            $table->string('landing_page')->nullable();
+            $table->timestamps(); // Adiciona as colunas created_at e updated_at
         });
     }
 
